35b09b5b2799132b819ce7a0c0184c3c1e888aa0
Bharat5965 <bharat.singhal5965@gmail.com>
2017-11-01 12:43:31 +0530

    Update rle.c

--- rle.c
+++ rle.c
@@ -1,12 +1,13 @@
 #include<stdio.h>
-char Rle[1000];
+#include<time.h>
+char Rle[1000000];
 int main ( )
 {
 	int i=0;
-	//char ch , str [ 100 ] ;
 	FILE *fptr;
     char filename[25], c;
-    printf("Enter the filename to open \n");
+    time_t t;
+	printf("Enter the filename to open \n");
     scanf("%s", filename);
     fptr = fopen(filename, "r");
     c = fgetc(fptr);
@@ -29,10 +30,15 @@ int main ( )
 }
 int RLEcompression()
 {
+	 
 	FILE *outfile;
-	int count=0,setcount=1,len;
+	clock_t start,end;
+	int count=0,setcount=1,len,orignal_bits=0,compressed_bits=0,bits_saved=0;
+	double timetaken;
 	outfile=fopen("out.txt","w");
+	start=clock();
 	len=strlen(Rle);
+	orignal_bits=8*len;
 	while(count<len)
 	{
     	if(Rle[count]==Rle[count+1])
@@ -42,18 +48,29 @@ int RLEcompression()
     	}
     	else
     	{
-   			printf("%c%d\t",Rle[count],setcount);
+   			printf("%c%d ",Rle[count],setcount);
     		fprintf(outfile, "%c%d ", Rle[count],setcount);
+    		compressed_bits=compressed_bits+(8*2);
     		setcount=1;
     		count++;
     	}
 	}
+	bits_saved=orignal_bits-compressed_bits;
+	printf("\norignal bits= %d\n",orignal_bits);
+	printf("Compressed bits= %d\n",compressed_bits);
+	printf("bits saved = %d\n\n",bits_saved);
+	end=clock();
+	timetaken=((double)(end-start))/CLOCKS_PER_SEC;
+	printf("%f\n",timetaken);
 }
 int RLEdecompression()
 {
 FILE *outfile;
+char filename1[25];
 int count,i,len,j;
-outfile=fopen("out1.txt","w");
+printf("Enter file for output");
+scanf("%s", filename1);
+outfile=fopen(filename1,"w");
 len=strlen(Rle);
 for(i=1;i<len;i=i+3)
 {
